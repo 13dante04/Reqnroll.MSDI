b062ffc992ff780fdc6a35eaf23ec5cab9e4145c
Robert Ban <Robert.Ban@newday.co.uk>
2024-11-20 12:22:54 +0000

    initial commit

---
+++ Clients/SomeHttpClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reqnroll.MSDI.Clients;
+
+public class SomeHttpClient
+{
+    private readonly HttpClient _httpClient;
+    
+    public SomeHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri("https://google.com");
+    }
+    public async Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content)
+    {
+        return await _httpClient.PostAsync(requestUri, content);
+    }
+}
